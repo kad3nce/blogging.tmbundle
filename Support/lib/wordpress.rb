d9083a783dafd5167fb20d2a403c9845dc3cb6ad
Jedidiah Hurt <jhurt@lighthousetwin.com>
2011-05-27 13:27:02 -0600

    Removing cruft.

--- Support/lib/wordpress.rb
+++ Support/lib/wordpress.rb
@@ -5,25 +5,21 @@ class WordPressClient < MetaWeblogClient
     super(*args)
   end
   
-  # √
   def get_page(blog_id, page_id, username, password)
     call("wp.getPage", "#{blog_id}", "#{page_id}", "#{username}", "#{password}")
   end
   alias_method :getPage, :get_page
   
-  # √
   def edit_page(blog_id, page_id, username, password, page, publish)
     call("wp.editPage", "#{blog_id}", "#{page_id}", "#{username}", "#{password}", page, publish)
   end
   alias_method :editPage, :edit_page
   
-  # √
   def new_page(blog_id, username, password, page, publish)
     call("wp.newPage", "#{blog_id}", "#{username}", "#{password}", page, publish)
   end
   alias_method :newPage, :new_page
   
-  # √
   def get_pages(blog_id, username, password, number)
     call("wp.getPages", "#{blog_id}", "#{username}", "#{password}", number)
   end
