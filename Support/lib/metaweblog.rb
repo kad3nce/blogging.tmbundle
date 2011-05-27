af639ad37bad69b969188b791144021e9b9c2e2f
Jedidiah Hurt <jhurt@lighthousetwin.com>
2011-05-27 10:27:37 -0600

    Replaced duplicated methods with method aliases.

--- Support/lib/metaweblog.rb
+++ Support/lib/metaweblog.rb
@@ -10,33 +10,25 @@ class MetaWeblogClient < XMLRPC::Client
   def get_post(post_id, username, password)
     call("metaWeblog.getPost", "#{post_id}", "#{username}", "#{password}")
   end
+  alias_method :getPost, :get_post
+  
   def edit_post(post_id, username, password, post, publish)
     call("metaWeblog.editPost", "#{post_id}", "#{username}", "#{password}", post, publish)
   end
+  alias_method :editPost, :edit_post
+  
   def new_post(blog_id, username, password, post, publish)
     call("metaWeblog.newPost", "#{blog_id}", "#{username}", "#{password}", post, publish)
   end
+  alias_method :newPost, :new_post
+  
   def get_recent_posts(blog_id, username, password, number)
     call("metaWeblog.getRecentPosts", "#{blog_id}", "#{username}", "#{password}", number)
   end
+  alias_method :getRecentPosts, :get_recent_posts
+  
   def new_media_object(blog_id, username, password, data)
     call("metaWeblog.newMediaObject", "#{blog_id}", "#{username}", "#{password}", data)
   end
-
-  # These methods are properCased to match the XMLRPC method names.
-  def getPost(post_id, username, password)
-    call("metaWeblog.getPost", "#{post_id}", "#{username}", "#{password}")
-  end
-  def editPost(post_id, username, password, post, publish)
-    call("metaWeblog.editPost", "#{post_id}", "#{username}", "#{password}", post, publish)
-  end
-  def newPost(blog_id, username, password, post, publish)
-    call("metaWeblog.newPost", "#{blog_id}", "#{username}", "#{password}", post, publish)
-  end
-  def getRecentPosts(blog_id, username, password, number)
-    call("metaWeblog.getRecentPosts", "#{blog_id}", "#{username}", "#{password}", number)
-  end
-  def newMediaObject(blog_id, username, password, data)
-    call("metaWeblog.newMediaObject", "#{blog_id}", "#{username}", "#{password}", data)
-  end
+  alias_method :newMediaObject, :new_media_object
 end
